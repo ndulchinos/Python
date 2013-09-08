34c31c7787fc2295d7390e53da07f8971ef9f0ea
ndulchinos <nicholasdulchinos@gmail.com>
2013-09-07 20:28:44 -0400

    Added scripts made at CERN
    These are some of my first python scripts created from scratch. They are
    commented to be easy to read for other python beginners.

---
+++ PythonProjects/analyze.py
@@ -0,0 +1,271 @@
+import math #this is to use the square root function
+EBATrips=open("EBATrips.txt","r") #opens "data" file for trips in EBA
+
+EBCTrips=open("EBCTrips.txt","r")
+
+LBATrips=open("LBATrips.txt","r")
+
+LBCTrips=open("LBCTrips.txt","r")
+
+EBAList=EBATrips.readlines() #makes a list of strings using the lines of EBATrips.txt
+
+EBCList=EBCTrips.readlines()
+
+LBAList=LBATrips.readlines()
+
+LBCList=LBCTrips.readlines()
+
+fo = open("output.txt","wb") #opens a file to write out the results
+
+#Compute averages
+
+Sum=0
+
+for line in EBAList:
+
+  x=line[6:] #get the last lines in the string, which should be the number of trips
+
+  y=int(x)   #convert this string into a number, so that we can use it like a number
+
+  Sum=Sum+y
+EBAavg = Sum / 64
+SigmaEBA = 0 #initialize the calculation for SigmaEBA
+
+for line in EBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  z = y - EBAavg #start computation of the standard deviation
+
+  z = z**(2) #this notation means to square z
+
+  SigmaEBA = SigmaEBA + z
+
+SigmaEBA = SigmaEBA / 64
+
+SigmaEBA = math.sqrt(SigmaEBA)
+
+print SigmaEBA #debug info
+
+print EBAavg
+TwoSigma = 2*(SigmaEBA)+ EBAavg #this is the number 2 sigma above the average
+
+fo.write("greater than 2 sigma \n")
+
+for line in EBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  if y >= TwoSigma: #picks out the modules with number of trips 2 sigma above average
+
+    fo.write(line[:5])
+
+    fo.write("\n") 
+ThreeSigma = 3*(SigmaEBA) + EBAavg #repeat the above work, but for 3 sigma
+ 
+fo.write("greater than 3 sigma \n")
+
+for line in EBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  if y >= ThreeSigma:
+    fo.write(line[:5])
+
+    fo.write("\n")
+
+Sum=0 #reset this variable, redo all computations for EBC, LBC, LBA:
+ 
+for line in EBCList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  Sum=Sum+y
+EBCavg = Sum / 64
+
+SigmaEBC = 0
+
+for line in EBCList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  z = y - EBCavg
+  z = z**(2)
+
+  SigmaEBC = SigmaEBC + z
+
+SigmaEBC = SigmaEBC / 64
+
+SigmaEBC = math.sqrt(SigmaEBC)
+
+print SigmaEBC
+
+print EBCavg
+
+TwoSigma = 2*(SigmaEBC)+ EBCavg
+
+fo.write("greater than 2 sigma \n")
+
+for line in EBCList:
+
+  x=line[6:]
+
+  y=int(x)
+  if y >= TwoSigma:
+
+    fo.write(line[:5])
+
+    fo.write("\n")
+ThreeSigma = 3*(SigmaEBC) + EBCavg
+
+fo.write("greater than 3 sigma \n")
+
+for line in EBCList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  if y >= ThreeSigma:
+    fo.write(line[:5])
+
+    fo.write("\n")
+
+Sum=0
+
+for line in LBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  Sum=Sum+y
+LBAavg = Sum / 64
+
+SigmaLBA = 0
+
+for line in LBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  z = y - LBAavg
+
+  z = z**2
+
+  SigmaLBA = SigmaLBA + z
+
+SigmaLBA = SigmaLBA / 64
+
+SigmaLBA = math.sqrt(SigmaLBA)
+
+print SigmaLBA
+
+print LBAavg
+
+TwoSigma = 2*(SigmaLBA)+ LBAavg
+
+fo.write("greater than 2 sigma \n")
+
+for line in LBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  if y >= TwoSigma:
+
+    fo.write(line[:5])
+
+    fo.write("\n")
+
+ThreeSigma = 3*(SigmaLBA) + LBAavg
+
+fo.write("greater than 3 sigma \n")
+
+for line in LBAList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  if y >= ThreeSigma:
+    fo.write(line[:5])
+
+    fo.write("\n")
+
+
+Sum=0
+
+for line in LBCList:
+  x=line[6:]
+
+  y=int(x)
+
+  Sum=Sum+y
+
+LBCavg = Sum / 64
+
+SigmaLBC = 0
+
+for line in LBCList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  z = y - LBCavg
+
+  z = z**(2)
+
+  SigmaLBC = SigmaLBC + z
+
+SigmaLBC = SigmaLBC / 64
+
+SigmaLBC = math.sqrt(SigmaLBC)
+
+print SigmaLBC
+
+print LBCavg
+
+TwoSigma = 2*(SigmaLBC)+ LBCavg
+
+fo.write("greater than 2 sigma \n")
+for line in LBCList:
+  x=line[6:]
+  y=int(x)
+
+  if y >= TwoSigma:
+
+    fo.write(line[:5])
+
+    fo.write("\n")
+
+ThreeSigma = 3*(SigmaLBC) + LBCavg
+
+fo.write("greater than 3 sigma \n")
+
+for line in LBCList:
+
+  x=line[6:]
+
+  y=int(x)
+
+  if y >= ThreeSigma:
+
+    fo.write(line[:5])
+
+    fo.write("\n")
+
+fo.close()
---
+++ PythonProjects/combine.py
@@ -0,0 +1,247 @@
+LBA11 = open("LBATrips11.txt","r") #opens the list of trips for LBA in 2011. the rest of these lines do the same for other partitions/years
+
+LBA12 = open("LBATrips12.txt","r")
+
+LBA13 = open("LBATrips13.txt","r")
+
+EBA11 = open("EBATrips11.txt","r")
+
+EBA12 = open("EBATrips12.txt","r")
+
+EBA13 = open("EBATrips13.txt","r")
+
+LBC11 = open("LBCTrips11.txt","r")
+
+EBC11 = open("EBCTrips11.txt","r")
+
+EBC12 = open("EBCTrips12.txt","r")
+
+EBC13 = open("EBCTrips13.txt","r")
+
+LBC12 = open("LBCTrips12.txt","r")
+
+LBC13 = open("LBCTrips13.txt","r")
+
+LBA = open("LBATrips.txt","wb") #opens the destination file 
+
+LBC = open("LBCTrips.txt","wb")
+
+EBA = open("EBATrips.txt","wb")
+
+EBC = open("EBCTrips.txt","wb")
+
+
+Trips11 = LBA11.readlines() #makes a list out of the lines of LBATrips11.txt
+
+Trips12 = LBA12.readlines()
+
+Trips13 = LBA13.readlines()
+
+
+for i in range(64): #loop over the modules in a partitions.
+ 
+ l = Trips11[i]
+ 
+ m = Trips12[i]
+
+  n = Trips13[i]
+
+  sNum11 = l[6:] #picks out the last characters in the line in consideration
+
+  sNum12 = m[6:]
+
+  sNum13 = n[6:]
+
+  Num11 = int(sNum11) #converts the last characters into numbers so that we can use them as numbers
+
+  Num12 = int(sNum12)
+
+  Num13 = int(sNum13)
+
+  Sum = Num11 + Num12 + Num13
+
+  if i < 9: #when i is <9, we want the string to be "0i" not "i"
+ 
+   num = str(i+1)
+
+    num = "0" + num
+
+  else:
+
+    num = str(i+1)
+
+  sum = str(Sum)
+
+  LBA.write("LBA") #write to the data file
+
+  LBA.write(num )
+  LBA.write(" ")
+
+  LBA.write(sum)
+
+  LBA.write("\n")
+
+
+Trips11 = EBA11.readlines() #restart the process, for EBA, then LBA, then LBC
+
+Trips12 = EBA12.readlines()
+
+Trips13 = EBA13.readlines()
+
+
+for i in range(64):
+
+  l = Trips11[i]
+
+  m = Trips12[i]
+
+  n = Trips13[i]
+
+  sNum11 = l[6:]
+
+  sNum12 = m[6:]
+
+  sNum13 = n[6:]
+
+  Num11 = int(sNum11)
+
+  Num12 = int(sNum12)
+
+  Num13 = int(sNum13)
+
+  Sum = Num11 + Num12 + Num13
+
+  if i < 9:
+
+    num = str(i+1)
+
+    num = "0" + num
+
+  else:
+
+    num = str(i+1)
+
+  sum = str(Sum)
+
+  EBA.write("EBA")
+
+  EBA.write(num )
+
+  EBA.write(" ")
+
+  EBA.write(sum)
+  EBA.write("\n")
+
+
+Trips11 = LBC11.readlines()
+
+Trips12 = LBC12.readlines()
+
+Trips13 = LBC13.readlines()
+
+
+for i in range(64):
+
+  l = Trips11[i]
+
+  m = Trips12[i]
+
+  n = Trips13[i]
+
+  sNum11 = l[6:]
+
+  sNum12 = m[6:]
+
+  sNum13 = n[6:]
+
+  Num11 = int(sNum11)
+
+  Num12 = int(sNum12)
+
+  Num13 = int(sNum13)
+
+  Sum = Num11 + Num12 + Num13
+
+  if i < 9:
+
+    num = str(i+1)
+
+    num = "0" + num
+
+  else:
+
+    num = str(i+1)
+
+  sum = str(Sum)
+
+  LBC.write("LBC")
+
+  LBC.write(num )
+
+  LBC.write(" ")
+
+  LBC.write(sum)
+
+  LBC.write("\n")
+
+
+Trips11 = EBC11.readlines()
+
+Trips12 = EBC12.readlines()
+
+Trips13 = EBC13.readlines()
+
+
+for i in range(64):
+
+  l = Trips11[i]
+
+  m = Trips12[i]
+
+  n = Trips13[i]
+
+  sNum11 = l[6:]
+
+  sNum12 = m[6:]
+
+  sNum13 = n[6:]
+
+  Num11 = int(sNum11)
+
+  Num12 = int(sNum12)
+
+  Num13 = int(sNum13)
+
+  Sum = Num11 + Num12 + Num13
+
+  if i < 9:
+    num = str(i+1)
+
+    num = "0" + num
+
+  else:
+
+    num = str(i+1)
+
+  sum = str(Sum)
+
+  EBC.write("EBC")
+
+  EBC.write(num )
+
+  EBC.write(" ")
+
+  EBC.write(sum)
+
+  EBC.write("\n")
+
+
+
+
+LBA.close
+
+EBA.close
+
+EBC.close
+
+LBC.close  
